b102c2cd05201e9faaddbee1e351299c6034e6cc
Subhesh Joshi <subhesh@Subheshs-MacBook-Pro.local>
2024-03-02 14:55:11 +0545

    Workflows implemented- 7

--- stocks/run_spiders.py
+++ stocks/run_spiders.py
@@ -3,6 +3,7 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from spiders.daily_trading import DailyTrading
 from spiders.proposed_dividents import ProposedDividendsSpider
+import sys
 
 
 def run_spiders():
@@ -19,3 +20,10 @@ def run_spiders():
 
 if __name__ == "__main__":
     run_spiders()
+
+
+imported_modules = sys.modules.keys()
+
+with open("imported_modules.txt", "w") as f:
+    for module in sorted(imported_modules):
+        f.write(module + "\n")
